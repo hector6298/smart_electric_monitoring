b9dacd439e53d483736145d9140aa4e9320c08aa
Hector Mejia Vallejo <hectormrejia@gmail.com>
2021-01-24 18:23:56 -0500

    fix add_dropdown for dynamic values

--- subscriberWebServer/dataviz/timeseries.py
+++ subscriberWebServer/dataviz/timeseries.py
@@ -3,12 +3,14 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
+import pandas as pd
 
 
 class Timeseries:
 
-    def __init__(self, data, timeseries_figure_id, timeseries_figure=None, **kwargs):
-        self.data = data
+    def __init__(self, columns, timeseries_figure_id, timeseries_figure=None, **kwargs):
+        self.columns = columns
+        self.data = pd.DataFrame(columns=columns)
         self._children = []
         self.timeseries_figure_id = timeseries_figure_id
         self.loading_id = self.timeseries_figure_id +'_loading'
@@ -18,7 +20,13 @@ class Timeseries:
         else:
 
             self.timeseries_figure = px.line(
-                data, **kwargs)
+                self.data, **kwargs)
+
+    def get_figure(self, **kwargs):
+        return px.line(self.data, **kwargs)
+        
+    def append_data(self, dictionary, ignore_index=True):
+        self.data = self.data.append(dictionary, ignore_index=ignore_index)
 
     def add_dropdrown(self, dropdown_label, dropdown_id, dropdown_options):
         self._children.extend([
@@ -68,6 +76,21 @@ class Timeseries:
                         
                         
                 
-                
-                
+class multiInstanceTimeseries(Timeseries):
 
+    def __init__(self, columns, timeseries_figure_id, timeseries_figure=None, **kwargs):
+        super().__init__(columns, timeseries_figure_id, timeseries_figure, **kwargs)
+        self.available_keys = dict()
+
+    def get_figure_by_key(self, key, **kwargs):
+        return px.line(self.available_keys[key], **kwargs)
+    
+    def get_available_keys(self):
+        return self.available_keys
+
+    def set_available_key(self, key):
+        self.available_keys[key] = pd.DataFrame(columns=self.columns)
+    
+    def append_data_by_key(self, key,  dictionary, ignore_index=True):
+        self.available_keys[key] = self.available_keys[key]\
+                                   .append(dictionary, ignore_index=ignore_index)
